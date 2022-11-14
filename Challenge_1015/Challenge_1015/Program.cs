84a390f778603075809f7f026588b80907fb88df
Joao Victor Maciel <joaovictormacieldefreitas@gmail.com>
2022-11-14 16:38:44 -0300

    Desafio 1015 Resolvido

---
+++ Challenge_1015/Challenge_1015/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Challenge_1015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] PrimeiraCoordenada = Console.ReadLine().Split(' ');
+            string[] SegundaCoordenada = Console.ReadLine().Split(' ');
+
+            double X1 = double.Parse(PrimeiraCoordenada[0], CultureInfo.InvariantCulture);
+            double Y1 = double.Parse(PrimeiraCoordenada[1], CultureInfo.InvariantCulture);
+
+            double X2 = double.Parse(SegundaCoordenada[0], CultureInfo.InvariantCulture);
+            double Y2 = double.Parse(SegundaCoordenada[1], CultureInfo.InvariantCulture);
+
+            double Calculo = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+
+            Console.WriteLine(Calculo.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
