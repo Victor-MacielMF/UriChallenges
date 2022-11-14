8280c4a17a70c1580c12f7e4c551490cd6816296
Joao Victor Maciel <joaovictormacieldefreitas@gmail.com>
2022-11-14 17:02:31 -0300

    Desafio 1016 Resolvido

---
+++ Challenge_1016/Challenge_1016/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Challenge_1016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int TempoPorKm = 2;
+            int Distancia = int.Parse(Console.ReadLine());
+
+            int Calculo = Distancia * TempoPorKm;
+
+            Console.WriteLine("{0} minutos", Calculo);
+        }
+    }
+}
