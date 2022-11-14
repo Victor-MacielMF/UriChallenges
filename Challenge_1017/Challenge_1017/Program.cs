9faed3b610f77441ac242aede464f0e78a497bdf
Joao Victor Maciel <joaovictormacieldefreitas@gmail.com>
2022-11-14 17:14:27 -0300

    Desafio 1017 Resolvido

---
+++ Challenge_1017/Challenge_1017/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Challenge_1017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double GastoCombustivel = 12.0;
+            int HorasGastas = int.Parse(Console.ReadLine());
+            int VelocidadeMedia = int.Parse(Console.ReadLine());
+
+            double DistanciaPercorrida = HorasGastas * VelocidadeMedia / GastoCombustivel;
+
+            Console.WriteLine(DistanciaPercorrida.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
